0865cf02788fcab28359863f59016aff82f4ca12
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-14 20:54:23 +0200

    Add Product class with properties and toString method

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,1 +1,24 @@
 
+
+public class Product{
+  private String id;
+  private String name;
+  private String version;
+  private double price;
+
+  public Product(String id, String name, String version, double price){
+    this.id = id;
+    this.name = name;
+    this.version = version;
+    this.price = price;
+  }
+
+  @Override
+  public String toString(){
+    return "Product: " + name + "\n"
+       + " ID: " + id + "\n"
+       + " Version: " + version +  "\n"
+       + " Price: " + price
+       + "\n" ;
+  }
+}
