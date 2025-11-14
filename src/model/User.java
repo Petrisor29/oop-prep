8107f6dd717569f98671be65abaf4a36df328bcb
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-14 20:54:52 +0200

    Add User class with properties and toString method

--- src/model/User.java
+++ src/model/User.java
@@ -1,1 +1,20 @@
 
+public class User{
+  private String name;
+  private String email;
+  private License license;
+
+  public User(String name, String email, License license){
+    this.name = name;
+    this.email = email;
+    this.license = license;
+  }
+
+  @Override
+  public String toString(){
+    return "User: " + name + "\n"
+       + " Email: " + email + "\n"
+       + "License: " + license
+       + "\n" ;
+  }
+}
