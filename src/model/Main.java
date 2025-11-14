639a9be277164c4bf890ec1f491bdb280d360810
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-14 20:59:18 +0200

    Refactor Main class to include main method

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,8 +1,12 @@
-Product laptop = new Product("12345", "Laptop", "1.0", 1000.00");
-License licenta = new License("123456789", "2023-12-31", Laptop);
-User john = new User("John Doe", "john.doe@example.com", license);
 
-System.out.println(Laptop);
-System.out.println(Licenta);
-System.out.println(John);
+public static void main(String[] args){
+  
+Product laptop = new Product("12345", "Laptop", "1.0", 1000.00);
+License licenta = new License("123456789", "2023-12-31", laptop);
+User john = new User("John Doe", "john.doe@example.com", licenta);
 
+System.out.println(laptop);
+System.out.println(licenta);
+System.out.println(john);
+
+}
